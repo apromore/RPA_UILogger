e6bd3341399cf79aefe6293626407df43cf0922d
Yiliyaer <y.yilihamu@leonardo.com.au>
2019-03-15 15:04:48 +1100

    windows bat

--- start_windows.bat
+++ start_windows.bat
@@ -6,6 +6,14 @@ cd append_http
 call npm install
 cd ..
 
-start npm start --prefix append_http
+
+echo "Please provide your user ID:"
+
+read userID
+
+echo "Starting action logger with userID: $userID"
+
+
+start npm start $userID --prefix append_http
 call npm start --prefix Excel_Addin
 
